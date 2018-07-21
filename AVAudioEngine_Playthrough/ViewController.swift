ca1903504cc71376150d33395944bf7731eda8ea
Douglas Adams <douglas.adams@me.com>
2018-07-21 13:49:35 -0400

    Working

--- AVAudioEngine_Playthrough/ViewController.swift
+++ AVAudioEngine_Playthrough/ViewController.swift
@@ -7,13 +7,16 @@
 //
 
 import Cocoa
+import AVFoundation
 
 class ViewController: NSViewController {
+
+  var engine = AVAudioEngine()
+  
 
   override func viewDidLoad() {
     super.viewDidLoad()
 
-    // Do any additional setup after loading the view.
   }
 
   override var representedObject: Any? {
@@ -22,6 +25,19 @@ class ViewController: NSViewController {
     }
   }
 
+  @IBAction func play(_ sender: NSButton) {
 
+//    let format = AVAudioFormat(standardFormatWithSampleRate: 48_000.0, channels: 2)
+    
+    engine.connect(engine.inputNode, to: engine.mainMixerNode, format: engine.inputNode.inputFormat(forBus: 0))
+    
+    // Start engine
+    do {
+      try engine.start()
+    }
+    catch {
+      print("oh no!")
+    }
+  }
 }
 
